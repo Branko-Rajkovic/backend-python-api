705ad1145ada3a9880fe363ddabcb16937a0bfd1
Branko <home.branko@gmail.com>
2025-01-25 12:10:52 +0100

    Postgresql added to project

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x,y):
+    """Add x and y return result"""
+    return x + y
+
+def substract(x,y):
+    """Substract and return result"""
+    return x - y
