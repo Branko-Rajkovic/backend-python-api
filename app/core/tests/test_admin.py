5b211d0e64906483e5974209ce7aff6baf68f09e
Branko <home.branko@gmail.com>
2025-01-26 00:19:42 +0100

    Create user API

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Test for Django admin"""
 
@@ -19,7 +20,7 @@ class AdminSiteTests(TestCase):
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email="user@example.com",
-            password="testpass123", 
+            password="testpass123",
             name="Test User",
         )
 
