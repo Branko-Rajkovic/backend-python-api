00c18237ee9ba2259eb7e463a3b7fbe4798f6e5e
Branko <home.branko@gmail.com>
2025-01-25 12:16:21 +0100

    Linting errors corrected

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y return result"""
     return x + y
 
-def substract(x,y):
+
+def substract(x, y):
     """Substract and return result"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,10 +12,10 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
-        res = calc.substract(10,15)
-        self.assertEqual(res, -5 )
+        res = calc.substract(10, 15)
+        self.assertEqual(res, -5)
